8c829e7c9b96e9ad463fb3c404310bfa2d5986c1
root <root@localhost.localdomain>
2014-08-14 20:55:40 +0800

    5

--- scrapoo.hs
+++ scrapoo.hs
@@ -18,40 +18,62 @@ import Data.Maybe
 import qualified Data.List as Ls
 import Data.Set.CharSet
 import Text.Groom
+import Data.String.Here
+import Syntax.Slot
 
 
 type Quote = String
-type Named = String
+type Name = String
+data Operator 
+	= OpSymbolic String 
+	| OpAlphabetic String --Including abbreviation
+	| OpComposed [Expr]
+	deriving (Eq,Read,Show,Ord)
 data Expr 
 	= ExSelector Char String
 	| ExRef String
 	| ExSlot
 	| ExBlock [Expr]
-	| ExBranch [Expr]
-	| ExInfixBinary Expr String Expr
-	| ExPrefix String [Expr]
-	| ExPostfix Expr [Expr] String
-	| ExNamed Expr String
-	| ExRegex
-	-- | ExDumb
+	-- | ExBranch [Expr] 
+	| ExInfixBinary Expr Operator Expr
+	| ExPrefix Operator [Name] [Expr]
+	| ExPostfix Expr [Expr] Operator [Name]
+	| ExNamed Expr Name
+	| ExRegex --TODO
 	deriving (Eq,Read,Show,Ord)
 
+--TODO: Spacing
+--TODO: Extraction
+
+-- $a .`+ 
+-- =
+-- _ $a ``+
+
 $(defineIsomorphisms ''Expr)
+$(defineIsomorphisms ''Operator)
+
+quote l r = between (text l) (text r)
+quote' l r = between (text l<*skipSpace) (skipSpace*>text r)
 
 operatorSymbols = stringCS "-+" --Read from the operator def table
 
 alpha = subset isAlpha <$> token
 num = subset isNumber <$> token
 symbol = subset (`elemCS` operatorSymbols) <$> token
-operator = many1 symbol <|> many1 alpha
 
+operator f
+	-- = opSimple <$> (many1 symbol <|> many1 alpha)
+	= opSymbolic <$> many1 symbol
+	<|> f (opAlphabetic <$> many1 alpha)
+	<|> f (opComposed <$> exprs)
 
+exprs = quote "[" "]" (many expr)
 
 --identifier = cons <$> alpha <*> many (alpha <|> num)
 identifier = many (alpha <|> num) 
 
 mkSelector delim = 
-	exSelector <$> pure d <*> between (text [d]) (text [d]) 
+	exSelector <$> pure d <*> quote [d] [d] 
 	(many $ subset (/=d) <$> token <|> (text [d,d] *> pure d))
 	where
 	d = delim
@@ -59,8 +81,6 @@ mkSelector delim =
 selectors = Ls.foldl1 (<|>) $ map mkSelector "/¶\\█○"
 
 arityMark n = text $ replicate n '`'
-
-preOp x = operator <*> text x
 
 nList::(Syntax f, Eq a) => Int -> f a -> f [a]
 nList n x = f n where
@@ -70,47 +90,71 @@ nList n x = f n where
 alts :: Alternative f => [f x] -> f x
 alts = Ls.foldl1 (<|>)
 
+naming = (text "@" ☆> identifier)
+names = many naming 
+
+infixr 6 <★>
+infixr 6 <☆>
+a <★> b = a <*> optSpace *> b
+a <☆> b = a <*> skipSpace *> b
+a ★> b = a *> optSpace *> b
+a ☆> b = a *> skipSpace *> b
+[many', many_] = map sepBy [optSpace, skipSpace]
+
+postfixNRest x n = nList (n-1) x <☆> arityMark n *> operator id <☆> names
+prefixN x n = exPrefix <$> operator id <*> arityMark n ☆> names <☆> nList n x
+
 expr::Syntax f => f Expr
 expr = e 0 
 	where
-	postfixNRest x n = nList (n-1) x <*> arityMark n *> operator
 	e::Syntax f => Int -> f Expr
 	e = \case
-		i@0 -> chainl1 (e (i+1)) operator exInfixBinary
+		i@0 -> chainl1 (e (i+1)) (operator $ quote "`" "`") exInfixBinary
 		i@1 -> let x = e $ i+1 in 
-			foldl exPostfix <$> x <*> many (alts $ map (postfixNRest x) [1,2,3])
-		i@2 -> foldl exNamed <$> e(i+1) <*> many (text "@" *> identifier)
+			foldl exPostfix <$> x <☆> many (alts $ map (postfixNRest x) [1,2,3])
+			<|> alts (map (prefixN x) [1,2,3])
+		i@2 -> foldl exNamed <$> e(i+1) <☆> names
 		_ -> expr'
 
 expr'::Syntax f => f Expr
 expr' = exRef <$> text "$" *> identifier
 	<|> exSlot <$> text "_"
 	<|> selectors
-	<|> alts (map prefixN [1,2,3]) 
-	<|> exBlock <$> between (text "{") (text "}") (many expr)
-	where
-	prefixN n = exPrefix <$> operator <*> arityMark n *> nList n expr 
+	-- <|> alts (map prefixN [1,2,3]) 
+	<|> exBlock <$> exprs
 
-
-test p p' x = do
+test p p' f x = do
 	putStrLn "========================================="
 	putStrLn x
 	--let p0 = Parser.Parser p
 	--let p1 = Printer.Printer p
 	let a = Parser.parse p x::[Expr]
-	putStrLn $ groom a
+	--putStrLn $ groom a
+	f a
 	let b = map (Printer.print p') a
-	putStrLn $ unlines $ catMaybes b
+	putStr $ unlines $ catMaybes b
 
 main = do
-	let t = test expr expr
-	t "$abcf@kkk+++/.whatever//.kkk/@abc@def+++○div○@123@@"
-	t "$aaa@kkk-_"
-	t "--`$abcf"
-	t "--``/.ww/@$"
-	t "$a$b``+$``+"
-	t "$1`+@5"
-	t "$--$@$@$@```x/a/@``y/b/@``z-/fff/@5"
+	let t = test expr expr print
+	let t' = test expr expr $ putStrLn.groom
+	let t0 = test expr expr $ \_->return ()
+	t0 "$abcf@kkk+++/.whatever//.kkk/@abc@def+++○div○@123@1@1"
+	t0 "$@+++/.whatever//.kkk/@@+++○div○@@@"
+	t0 "$aaa@kkk-_"
+	t0 "--`$abcf"
+	t0 "--``/.ww/@1$1"
+	t0 "$a$b``+$1``+"
+	t0 "$1`+@5"
+	t0 "+`$1@5"
+	t0 "$1--$2@3$4@5$5@6```x/a/@1``y/b/@2``z-/fff/@5"
+	t0 "$--$@$@$@```x/a/@``y/b/@``z-/fff/@"
+	t0 "+``$1$2$3``-"
+	t0 "+``@x$1@a$2@b$3@c$4@d```-@z"
+	t0 "$`a`$"
+	t0 "$a$``a"
+
+	t' [here|$`[_`a]
+	|]
 
 	t "--``$abcd"
 	t ""
